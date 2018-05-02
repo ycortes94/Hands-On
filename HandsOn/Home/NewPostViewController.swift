7f2acdd2a7547b9c68f78400a1ff7fac2ad12c2d
Rich Chau <richsensations@Richs-Mac.local>
2018-05-01 20:49:14 -0700

    Added test create new task form

--- HandsOn/Home/NewPostViewController.swift
+++ HandsOn/Home/NewPostViewController.swift
@@ -12,25 +12,136 @@ import Firebase
 
 class NewPostViewController:UIViewController, UITextViewDelegate {
     
+    
     let backgroundView : UIView = {
         
         let view = UIView()
         view.backgroundColor = UIColor(red: 0.976, green: 0.977, blue: 0.976, alpha: 1.0)
         view.layer.cornerRadius = 7
-        
         view.translatesAutoresizingMaskIntoConstraints = false
-        
         return view
         
     }()
     
     
-    let taskInputStackView : UIStackView = {
-        let stack = UIStackView()
-        stack.backgroundColor = UIColor.clear
-        
-        return stack
+    
+    
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Title of Task"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
+    
+    let titleTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter a title"
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textField.textColor = UIColor.black
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.black.cgColor
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false;
+        return textField
+    }()
+    
+    let descLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Task Description"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter a description"
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textField.textColor = UIColor.black
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.black.cgColor
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false;
+        return textField
+    }()
+    
+    let priceLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Offered Price"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let priceTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter a price"
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textField.textColor = UIColor.black
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.black.cgColor
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false;
+        return textField
+    }()
+    
+    let locationLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Location of Task"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let locationTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter a location"
+        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textField.textColor = UIColor.black
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = UIColor.black.cgColor
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false;
+        return textField
+    }()
+    
+    let addImagesLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Add Images"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    let postButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Preview and Post", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.semibold)
+        button.titleLabel?.textColor = UIColor.black
+        button.backgroundColor = UIColor(red: 0.408, green: 0.408, blue: 0.408, alpha: 1.0)
+        button.layer.cornerRadius = 3
+        button.addTarget(self, action: #selector(testPrint), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    
     
     let textView : UITextView = {
         let view = UITextView()
@@ -114,11 +225,24 @@ class NewPostViewController:UIViewController, UITextViewDelegate {
         assignbackground()
         
         
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, titleTextField, descLabel,descTextField, priceLabel, priceTextField, locationLabel, locationTextField, addImagesLabel, postButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 1
+        
+        
         view.addSubview(backgroundView)
+        view.addSubview(stackView)
         view.addSubview(textView)
         view.addSubview(doneButton)
         view.addSubview(cancelButton)
         setUpLayout()
+        
+        stackView.leftAnchor.constraint(equalTo: backgroundView.leftAnchor, constant: 30).isActive = true
+        stackView.rightAnchor.constraint(equalTo: backgroundView.rightAnchor, constant: -30).isActive = true
+        stackView.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 20).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: -20).isActive = true
         
         textView.delegate = self
         
@@ -130,6 +254,7 @@ class NewPostViewController:UIViewController, UITextViewDelegate {
     
     func setUpLayout(){
         
+        
         backgroundView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         backgroundView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         backgroundView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
@@ -137,8 +262,9 @@ class NewPostViewController:UIViewController, UITextViewDelegate {
         backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 13).isActive = true
         backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -13).isActive = true
         
+        
         textView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        textView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        textView.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 100).isActive = true
         textView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         textView.widthAnchor.constraint(equalToConstant: 200).isActive = true
         
@@ -151,6 +277,26 @@ class NewPostViewController:UIViewController, UITextViewDelegate {
         doneButton.topAnchor.constraint(equalTo: cancelButton.bottomAnchor, constant: 5).isActive = true
         doneButton.widthAnchor.constraint(equalToConstant: 75).isActive = true
         doneButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let labelHeight : CGFloat = 30
+        titleLabel.heightAnchor.constraint(equalToConstant: labelHeight).isActive = true
+        descLabel.heightAnchor.constraint(equalToConstant: labelHeight).isActive = true
+        priceLabel.heightAnchor.constraint(equalToConstant: labelHeight).isActive = true
+        locationLabel.heightAnchor.constraint(equalToConstant: labelHeight).isActive = true
+        addImagesLabel.heightAnchor.constraint(equalToConstant: labelHeight).isActive = true
+        
+        let fieldHeight : CGFloat = 30
+        titleTextField.heightAnchor.constraint(equalToConstant: fieldHeight).isActive = true
+        priceTextField.heightAnchor.constraint(equalToConstant: fieldHeight).isActive = true
+        locationTextField.heightAnchor.constraint(equalToConstant: fieldHeight).isActive = true
+        
+        postButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+    
+
+    }
+    
+    @objc func testPrint(){
+        print(1)
     }
     
     func assignbackground(){
